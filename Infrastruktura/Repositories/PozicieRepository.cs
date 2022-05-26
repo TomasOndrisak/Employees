e5e3311b80d7b0ded4a9047fdebb852f872d865a
TomasOndrisak <104638159+TomasOndrisak@users.noreply.github.com>
2022-05-26 09:06:15 +0200

    push

--- Infrastruktura/Repositories/PozicieRepository.cs
+++ Infrastruktura/Repositories/PozicieRepository.cs
@@ -67,10 +67,7 @@ namespace Infrastruktura.Repositories
             
             var pozicie = await _context.Pozicie.FindAsync(id);
             
-         /*  var query = from q in _context.Zamestnanci
-                        where q.IdPozicie==id
-                        select q;
-        */
+         
             var qeuryy =  _context.Zamestnanci.Any(e => e.IdPozicie == id);
             
             
@@ -85,64 +82,5 @@ namespace Infrastruktura.Repositories
             
             await _context.SaveChangesAsync();
         }
-
-
-        /*   
-         *   Predosle
-         *   
-         *   public async Task<IActionResult> PutPozicie(int id, Pozicie pozicie)
-             {
-                 if (id != pozicie.Id)
-                 {
-                     return BadRequest();
-                 }
-
-                 _context.Entry(pozicie).State = EntityState.Modified;
-
-                 try
-                 {
-                     await _context.SaveChangesAsync();
-                 }
-                 catch (DbUpdateConcurrencyException)
-                 {
-                     if (!PozicieExists(id))
-                     {
-                         return NotFound();
-                     }
-                     else
-                     {
-                         throw;
-                     }
-                 }
-
-                 return NoContent();
-             }*/
-        /*
-
-        public async Task<ActionResult<Pozicie>> PostPozicie(Pozicie pozicie)
-        {
-            _context.Pozicie.Add(pozicie);
-            await _context.SaveChangesAsync();
-
-            return CreatedAtAction("GetPozicie", new { id = pozicie.Id }, pozicie);
-        }
-        public async Task<IActionResult> DeletePozicie(int id)
-        {
-            var pozicie = await _context.Pozicie.FindAsync(id);
-            if (pozicie == null)
-            {
-                return NotFound();
-            }
-
-            _context.Pozicie.Remove(pozicie);
-            await _context.SaveChangesAsync();
-
-            return NoContent();
-        }
-
-        private bool PozicieExists(int id)
-        {
-            return _context.Pozicie.Any(e => e.Id == id);
-        }*/
     }
 }
